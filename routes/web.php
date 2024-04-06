149502d1f858fb9d5f6b33d8fe6112e81e5a058b
Lucía huerta <luciaxonihelli@gmail.com>
2024-04-06 15:17:43 -0600

    UPDATE: se agregan rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Charges\ChargeDerivateController;
+use App\Http\Controllers\Charges\ChargeSubDerivateController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Room\ElectricChargeController;
+use App\Http\Controllers\Room\RoomController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+});
+
+Route::prefix('room')->name('room.')->group(function () {
+    Route::resource('rooms', RoomController::class);
+    Route::resource('electric-charge', ElectricChargeController::class);
+});
+
+Route::prefix('charge')->name('charge.')->group(function () {
+    Route::resource('directive', ChargeDerivateController::class);
+    Route::resource('sub-directive', ChargeSubDerivateController::class);
 });
 
 require __DIR__ . '/auth.php';
