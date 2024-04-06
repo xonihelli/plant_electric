edec37f1055ef491fa34c42c6df7999ad925eaa0
Erik Gomez <erikgomez398@gmail.com>
2024-04-06 15:05:18 -0600

    Agregar controladores de carga derivada y carga subderivada en la carpeta de Cargas Agregar controladores de carga eléctrica y habitación en la carpeta de Habitaciones

---
+++ app/Http/Controllers/Charges/ChargeDerivateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Charges;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChargeDerivateController extends Controller
+{
+    //
+}
