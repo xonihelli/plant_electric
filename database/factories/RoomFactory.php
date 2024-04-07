6906be0b47d2f253b6e2b09009d014a7a8834079
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 16:59:59 -0600

    Agrega factory RoomFactory.php

---
+++ database/factories/RoomFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Room;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Room>
+ */
+class RoomFactory extends Factory
+{
+    /**
+     * Summary of model
+     * @var
+     */
+    protected $model = Room::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+        ];
+    }
+}
