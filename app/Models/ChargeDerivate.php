526e871783afd7d9df4a42023569c952889ef75a
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 16:59:33 -0600

    ACTUALIZACIÓN: Se agrega SoftDeletes trait en ChargeDerivate.php

--- app/Models/ChargeDerivate.php
+++ app/Models/ChargeDerivate.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class ChargeDerivate extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * Summary of table
@@ -29,7 +30,6 @@ class ChargeDerivate extends Model
     ];
 
     /*-------------------------------- RELACIONES ------------------------------*/
-
     /**
      * Summary of electricCharge
      * @return mixed
