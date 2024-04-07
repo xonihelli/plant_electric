23319a9d4e00d1cbbe81a1f31ed6e4b7f540310e
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:21 -0600

    ACTUALIZACIÓN: cambios en el formulario de registro
    ACTUALIZACIÓN: cambios en el controlador de derivados de carga
    
    ACTUALIZACIÓN: cambios en el controlador de derivados de carga
    
    ACTUALIZACIÓN: cambios en el controlador de derivados de carga
    
    ACTUALIZACIÓN: cambios en el controlador de derivados de carga
    
    UPDATE: reubicacion
    
    UPDATE: cambios de librerias
    
    UPDATE: index de cuartos
    
    UPDATE: cambios en pagionas auth
    
    UPDATE: cambio de layout web

--- resources/js/Pages/Charges/ChargeDerivate/Create.vue
+++ resources/js/Pages/Charges/ChargeDerivate/Create.vue
@@ -1,14 +1,45 @@
-<template>
-    <div>
-        
-    </div>
-</template>
 
 <script setup>
+import { router, useForm } from "@inertiajs/vue3";
 
+const props = defineProps({
+  electricCharges: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const form = useForm({
+  electric_charge_id: "",
+  name_technical: "",
+  name: "",
+  kw: "",
+  a: "",
+});
+
+const sendForm = () => {
+  form.submit("post", route("charge.directive.store"), {
+    onFinish: () => {
+      if (Object.keys(form.errors).length) {
+        Swal.fire({
+          icon: "error",
+          title: "Hay errores en el formulario",
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "La información de la carga derivada ha sido guardada",
+        }).then(() => {
+          router.push(route("charge.directive.index"));
+        });
+      }
+    },
+  });
+};
 </script>
+<template>
+  <div></div>
+</template>
 
 <style lang="scss" scoped>
-
-
 </style>
