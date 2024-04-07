3fb0101477f7c4a7a3ea9f7208df42872bd33465
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 13:20:16 -0600

    ACTUALIZACIÓN: Se corrige diseño de la página de inicio de sesión

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,37 +1,45 @@
 <script setup>
-import WebLayout from '@/Layouts/WebLayout.vue';
+import WebLayout from "@/Layouts/WebLayout.vue";
 
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <WebLayout>
-        <div class="authincation min-h-[100vh] h-full flex">
-            
-            <div class="container-fluid h-full pt-0 w-full px-[12.5px]">
-                <div class="row h-[100vh]">
-                    <div class="lg:w-1/2 w-full mx-auto self-center">
-                        <div class="login-form mx-auto max-w-[600px] lg:px-[50px] lg:py-0 sm:p-[25px] p-[14px]">
-                            
-                            <div class="flex justify-center w-full">
-                                <Link href="/">
-                                    <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-                                </Link>
-                            </div>
+  <WebLayout>
+    <div class="authincation min-h-[100vh] h-full flex">
+      <div class="container-fluid h-full pt-0 w-full px-[12.5px]">
+        <div class="row h-[100vh]">
+          <div class="lg:w-1/2 w-full mx-auto self-center">
+            <div
+              class="login-form mx-auto max-w-[600px] lg:px-[50px] lg:py-0 sm:p-[25px] p-[14px]"
+            >
+              <div class="flex justify-center w-full">
+                <Link :href="route('dashboard')">
+                  <ApplicationLogo
+                    class="w-20 h-20 fill-current text-gray-500"
+                  />
+                </Link>
+              </div>
 
-                            <slot />
-                        </div>
-                    </div>
-                    <div class="lg:w-1/2 w-full">
-                        <div class="pages-left h-full bg-white dark:bg-[#242424]">
-                            <div class="login-media text-center flex items-center justify-center h-full">
-                                <img src="assets/images/login.png" alt="" class="max-lg:w-[60%] w-[70%] inline-block">
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <slot />
             </div>
+          </div>
+          <div class="lg:w-1/2 w-full">
+            <div class="pages-left h-full bg-white dark:bg-[#242424]">
+              <div
+                class="login-media text-center flex items-center justify-center h-full"
+              >
+                <img
+                  src="assets/images/login.png"
+                  alt=""
+                  class="max-lg:w-[60%] w-[70%] inline-block"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    </WebLayout>
+      </div>
+    </div>
+  </WebLayout>
 </template>
