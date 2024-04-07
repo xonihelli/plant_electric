c24e860f16478d5d62727fd1659247dc9b68f8ea
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:59 -0600

    ACTUALIZACIÓN: cambios en el formulario de edición de cuarto eléctrico

--- resources/js/Pages/Rooms/Room/Edit.vue
+++ resources/js/Pages/Rooms/Room/Edit.vue
@@ -1,14 +1,51 @@
-<template>
-    <div>
-        
-    </div>
-</template>
 
 <script setup>
+import { router, useForm } from "@inertiajs/vue3";
 
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
+  idData: {
+    type: Number,
+  },
+});
+
+const form = useForm({
+  name: props.data.name ?? "",
+  description: props.data.description ?? "",
+});
+
+const sendForm = () => {
+  const formData = new FormData();
+
+  formData.append("name", form.name);
+  formData.append("description", form.description);
+
+  form.submit("put", route("rooms.room.update", props.idData), {
+    body: formData,
+    onFinish: () => {
+      if (Object.keys(form.errors).length) {
+        Swal.fire({
+          icon: "error",
+          title: "Hay errores en el formulario",
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "La información del cuarto eléctrico ha sido actualizada",
+        }).then(() => {
+          router.push(route("rooms.room.index"));
+        });
+      }
+    },
+  });
+};
 </script>
 
+<template>
+  <div></div>
+</template>
 <style lang="scss" scoped>
-
-
 </style>
