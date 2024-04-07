8f34ef4b682d44a383af17f59fd016646490aef0
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:33 -0600

    ACTUALIZACIÓN: cambios en el formulario de creación de carga subderivada

--- resources/js/Pages/Charges/ChargeSubDerivate/Create.vue
+++ resources/js/Pages/Charges/ChargeSubDerivate/Create.vue
@@ -1,14 +1,51 @@
-<template>
-    <div>
-        
-    </div>
-</template>
 
 <script setup>
+import { router, useForm } from "@inertiajs/vue3";
 
+const props = defineProps({
+  chargeDerivates: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+const form = useForm({
+  charge_derivate_id: "",
+  name: "",
+  kw: "",
+  a: "",
+  brand: "",
+  model: "",
+  capacity: "",
+  location: "",
+  lightning_discharge: "",
+  surge: "",
+});
+
+const sendForm = () => {
+  form.submit("post", route("charge.sub-directive.store"), {
+    onFinish: () => {
+      if (Object.keys(form.errors).length) {
+        Swal.fire({
+          icon: "error",
+          title: "Hay errores en el formulario",
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "La información de la carga subderivada ha sido guardada",
+        }).then(() => {
+          router.push(route("charge.sub-directive.index"));
+        });
+      }
+    },
+  });
+};
 </script>
+<template>
+  <div></div>
+</template>
+
 
 <style lang="scss" scoped>
-
-
 </style>
