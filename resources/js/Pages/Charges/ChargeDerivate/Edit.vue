831297a11fd41bc93d3117d4763781e64b09f121
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:25 -0600

    ACTUALIZACIÓN: cambios en el formulario de edición de carga derivada

--- resources/js/Pages/Charges/ChargeDerivate/Edit.vue
+++ resources/js/Pages/Charges/ChargeDerivate/Edit.vue
@@ -1,14 +1,61 @@
-<template>
-    <div>
-        
-    </div>
-</template>
 
 <script setup>
+import { router, useForm } from "@inertiajs/vue3";
 
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
+  electricCharges: {
+    type: Object,
+    default: () => ({}),
+  },
+  idData: {
+    type: Number,
+  },
+});
+
+const form = useForm({
+  electric_charge_id: props.data.electric_charge_id ?? "",
+  name_technical: props.data.name_technical ?? "",
+  name: props.data.name ?? "",
+  kw: props.data.kw ?? "",
+  a: props.data.a ?? "",
+});
+
+const sendForm = () => {
+  const formData = new FormData();
+
+  formData.append("electric_charge_id", form.electric_charge_id);
+  formData.append("name", form.name);
+  formData.append("kw", form.kw);
+  formData.append("a", form.a);
+
+  form.submit("put", route("charge.directive.update", props.idData), {
+    body: formData,
+    onFinish: () => {
+      if (Object.keys(form.errors).length) {
+        Swal.fire({
+          icon: "error",
+          title: "Hay errores en el formulario",
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "La información de la carga derivada ha sido actualizada",
+        }).then(() => {
+          router.push(route("charge.directive.index"));
+        });
+      }
+    },
+  });
+};
 </script>
+<template>
+  <div></div>
+</template>
+
 
 <style lang="scss" scoped>
-
-
 </style>
