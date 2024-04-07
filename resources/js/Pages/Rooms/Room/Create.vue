6e83482b15975eb21af707a1e573812e04cfbb8b
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:55 -0600

    ACTUALIZACIÓN: cambios en el formulario de creación de cuarto eléctrico

--- resources/js/Pages/Rooms/Room/Create.vue
+++ resources/js/Pages/Rooms/Room/Create.vue
@@ -1,14 +1,34 @@
-<template>
-    <div>
-        
-    </div>
-</template>
 
 <script setup>
+import { router, useForm } from "@inertiajs/vue3";
 
+const form = useForm({
+  name: "",
+  description: "",
+});
+
+const sendForm = () => {
+  form.submit("post", route("rooms.room.store"), {
+    onFinish: () => {
+      if (Object.keys(form.errors).length) {
+        Swal.fire({
+          icon: "error",
+          title: "Hay errores en el formulario",
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "La información del cuarto eléctrico ha sido guardada",
+        }).then(() => {
+          router.push(route("rooms.room.index"));
+        });
+      }
+    },
+  });
+};
 </script>
-
+<template>
+  <div></div>
+</template>
 <style lang="scss" scoped>
-
-
 </style>
