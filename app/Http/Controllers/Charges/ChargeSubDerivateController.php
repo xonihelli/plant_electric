0babc901d5d4ed11f2e7c25e36f59c94a2c729ff
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 11:22:01 -0600

    ACTUALIZACIÓN: cambios en el controlador de derivados de carga

--- app/Http/Controllers/Charges/ChargeSubDerivateController.php
+++ app/Http/Controllers/Charges/ChargeSubDerivateController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Charges;
 
 use App\Http\Controllers\Controller;
+use App\Models\ChargeSubDerivate;
+use App\Models\ChargeDerivate;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChargeSubDerivateController extends Controller
 {
@@ -12,7 +15,14 @@ class ChargeSubDerivateController extends Controller
      */
     public function index()
     {
-        //
+        $chargeSubDerivates = ChargeSubDerivate::all();
+
+        return Inertia::render(
+            'Charges/ChargeSubDerivate/Index',
+            [
+                'data' => $chargeSubDerivates,
+            ]
+        );
     }
 
     /**
@@ -20,7 +30,14 @@ class ChargeSubDerivateController extends Controller
      */
     public function create()
     {
-        //
+        $chargeDerivates = ChargeDerivate::all();
+
+        return Inertia::render(
+            'Charges/ChargeSubDerivate/Create',
+            [
+                'chargeDerivates' => $chargeDerivates,
+            ]
+        );
     }
 
     /**
@@ -28,7 +45,57 @@ class ChargeSubDerivateController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(
+            [
+                'charge_derivate_id' => 'required|integer',
+                'name' => 'required|string|max:255',
+                'kw' => 'required|numeric',
+                'a' => 'required|numeric',
+                'brand' => 'required|string|max:255',
+                'model' => 'required|string|max:255',
+                'capacity' => 'required|numeric',
+                'location' => 'required|string|max:255',
+                'lightning_discharge' => 'required|numeric',
+                'surge' => 'required|numeric',
+            ],
+            [
+                'charge_derivate_id.required' => 'El campo charge_derivate_id es obligatorio',
+                'charge_derivate_id.integer' => 'El campo charge_derivate_id debe ser un número entero',
+                'name.required' => 'El campo name es obligatorio',
+                'name.string' => 'El campo name debe ser una cadena de texto',
+                'kw.required' => 'El campo kw es obligatorio',
+                'kw.numeric' => 'El campo kw debe ser un número',
+                'a.required' => 'El campo a es obligatorio',
+                'a.numeric' => 'El campo a debe ser un número',
+                'brand.required' => 'El campo brand es obligatorio',
+                'brand.string' => 'El campo brand debe ser una cadena de texto',
+                'model.required' => 'El campo model es obligatorio',
+                'model.string' => 'El campo model debe ser una cadena de texto',
+                'capacity.required' => 'El campo capacity es obligatorio',
+                'capacity.numeric' => 'El campo capacity debe ser un número',
+                'location.required' => 'El campo location es obligatorio',
+                'location.string' => 'El campo location debe ser una cadena de texto',
+                'lightning_discharge.required' => 'El campo lightning_discharge es obligatorio',
+                'lightning_discharge.numeric' => 'El campo lightning_discharge debe ser un número',
+                'surge.required' => 'El campo surge es obligatorio',
+                'surge.numeric' => 'El campo surge debe ser un número',
+            ]
+        );
+
+        $charge = new ChargeSubDerivate();
+        $charge->charge_derivate_id = $request->charge_derivate_id;
+        $charge->name = $request->name;
+        $charge->kw = $request->kw;
+        $charge->a = $request->a;
+        $charge->brand = $request->brand;
+        $charge->model = $request->model;
+        $charge->capacity = $request->capacity;
+        $charge->location = $request->location;
+        $charge->lightning_discharge = $request->lightning_discharge;
+        $charge->surge = $request->surge;
+        $charge->save();
+
+        return redirect()->route('charge.sub-directive.index');
     }
 
     /**
@@ -44,7 +111,16 @@ class ChargeSubDerivateController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $chargeSubDerivate = ChargeSubDerivate::find($id);
+        $chargeDerivates = ChargeDerivate::all();
+
+        return Inertia::render(
+            'Charges/ChargeSubDerivate/Edit',
+            [
+                'data' => $chargeSubDerivate,
+                'chargeDerivates' => $chargeDerivates,
+            ]
+        );
     }
 
     /**
@@ -52,7 +128,57 @@ class ChargeSubDerivateController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate(
+            [
+                'charge_derivate_id' => 'required|integer',
+                'name' => 'required|string|max:255',
+                'kw' => 'required|numeric',
+                'a' => 'required|numeric',
+                'brand' => 'required|string|max:255',
+                'model' => 'required|string|max:255',
+                'capacity' => 'required|numeric',
+                'location' => 'required|string|max:255',
+                'lightning_discharge' => 'required|numeric',
+                'surge' => 'required|numeric',
+            ],
+            [
+                'charge_derivate_id.required' => 'El campo charge_derivate_id es obligatorio',
+                'charge_derivate_id.integer' => 'El campo charge_derivate_id debe ser un número entero',
+                'name.required' => 'El campo name es obligatorio',
+                'name.string' => 'El campo name debe ser una cadena de texto',
+                'kw.required' => 'El campo kw es obligatorio',
+                'kw.numeric' => 'El campo kw debe ser un número',
+                'a.required' => 'El campo a es obligatorio',
+                'a.numeric' => 'El campo a debe ser un número',
+                'brand.required' => 'El campo brand es obligatorio',
+                'brand.string' => 'El campo brand debe ser una cadena de texto',
+                'model.required' => 'El campo model es obligatorio',
+                'model.string' => 'El campo model debe ser una cadena de texto',
+                'capacity.required' => 'El campo capacity es obligatorio',
+                'capacity.numeric' => 'El campo capacity debe ser un número',
+                'location.required' => 'El campo location es obligatorio',
+                'location.string' => 'El campo location debe ser una cadena de texto',
+                'lightning_discharge.required' => 'El campo lightning_discharge es obligatorio',
+                'lightning_discharge.numeric' => 'El campo lightning_discharge debe ser un número',
+                'surge.required' => 'El campo surge es obligatorio',
+                'surge.numeric' => 'El campo surge debe ser un número',
+            ]
+        );
+
+        $charge = ChargeSubDerivate::find($id);
+        $charge->charge_derivate_id = $request->charge_derivate_id;
+        $charge->name = $request->name;
+        $charge->kw = $request->kw;
+        $charge->a = $request->a;
+        $charge->brand = $request->brand;
+        $charge->model = $request->model;
+        $charge->capacity = $request->capacity;
+        $charge->location = $request->location;
+        $charge->lightning_discharge = $request->lightning_discharge;
+        $charge->surge = $request->surge;
+        $charge->save();
+
+        return redirect()->route('charge.sub-directive.index');
     }
 
     /**
@@ -60,6 +186,9 @@ class ChargeSubDerivateController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $charge = ChargeSubDerivate::find($id);
+        $charge->delete();
+
+        return redirect()->route('charge.sub-directive.index');
     }
 }
