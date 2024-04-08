ee84b32309c37a9bd081339abab3c48bead1ce94
Erik Gomez <erikgomez398@gmail.com>
2024-04-07 18:58:58 -0600

    Agrega cambios en la página de edición de cuartos eléctricos y carga eléctrica

--- resources/js/Pages/Charges/ChargeDerivate/Create.vue
+++ resources/js/Pages/Charges/ChargeDerivate/Create.vue
@@ -32,7 +32,7 @@ const sendForm = () => {
       } else {
         Swal.fire({
           icon: "success",
-          title: "La información de la carga derivada ha sido guardada",
+          title: "La información del tablero de distribuición ha sido creada",
         }).then(() => {
           location.href("charge.directive.index");
         });
@@ -79,7 +79,7 @@ let room = {
       <div
         class="card-header flex justify-between px-5 pt-6 relative z-[2] pb-0"
       >
-        <h2 class="card-title text-base">Detalles de cuarto</h2>
+        <h2 class="card-title text-base">Detalles del transformador</h2>
       </div>
       <div class="card-body sm:p-5 p-4 sm:pb-0 pb-0">
         <div class="flex flex-wrap w-full">
@@ -145,10 +145,10 @@ let room = {
       <div
         class="card-header sm:flex justify-between items-center px-5 pt-6 relative z-[2] pb-0 block"
       >
-        <h4 class="card-title capitalize">Agregar cargas derivadas</h4>
+        <h4 class="card-title capitalize">Agregar Tablero de distribuición</h4>
 
         <Link
-          :href="route('rooms.room.show', { room: 1 })"
+          :href="route('charge.directive.index')"
           class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
         >
           <i class="fa-solid fa-arrow-left"></i>
@@ -156,76 +156,92 @@ let room = {
         </Link>
       </div>
       <div class="sm:p-5 p-4">
-        <div class="space-y-4">
-          <div>
-            <InputLabel for="electric_charge_id" value="Carga eléctrica" />
-            <TextInput
-              id="electric_charge_id"
-              v-model="form.electric_charge_id"
-              type="text"
-              autocomplete="off"
-            />
-            <InputError :message="form.electric_charge_id" class="mt-2" />
-          </div>
+        <form @submit.prevent="sendForm" class="form">
+          <div class="space-y-4">
+            <div>
+              <InputLabel
+                for="electric_charge_id"
+                value="Transformador al que pertenece"
+              />
+              <select
+                class="form-select relative text-[13px] text-body-color h-[2.813rem] border border-b-color block rounded-md py-1.5 px-3 duration-500 focus:border-primary dark:hover:border-b-color outline-none w-full input-default"
+                id="room_id"
+                v-model="form.electric_charge_id"
+              >
+                <option value="" disabled selected>
+                  Selecciona un transformador electrico al que pertenece el
+                  trablero de distribución
+                </option>
+                <option
+                  v-for="electricCharge in electricCharges"
+                  :key="electricCharge.id"
+                  :value="electricCharge.id"
+                >
+                  {{ electricCharge.name }}
+                </option>
+              </select>
+              <InputError :message="form.errors.electric_charge_id" />
+            </div>
 
-          <div class="flex sm:flex-wrap md:flex-nowrap space-x-2">
-            <div class="w-full md:w-1/2">
-              <InputLabel for="name_technical" value="Nombre técnico" />
-              <TextInput
-                id="name_technical"
-                v-model="form.name_technical"
-                type="text"
-                autocomplete="off"
-              />
-              <InputError :message="form.name_technical" class="mt-2" />
+            <div class="flex sm:flex-wrap md:flex-nowrap space-x-2">
+              <div class="w-full md:w-1/2">
+                <InputLabel for="name_technical" value="Nombre técnico" />
+                <TextInput
+                  id="name_technical"
+                  v-model="form.name_technical"
+                  type="text"
+                  autocomplete="off"
+                />
+                <InputError :message="form.errors.name_technical" />
+              </div>
+              <div class="w-full md:w-1/2">
+                <InputLabel for="name" value="Nombre" />
+                <TextInput
+                  id="name"
+                  v-model="form.name"
+                  type="text"
+                  autocomplete="off"
+                />
+                <InputError :message="form.errors.name" />
+              </div>
             </div>
-            <div class="w-full md:w-1/2">
-              <InputLabel for="name" value="Nombre" />
-              <TextInput
-                id="name"
-                v-model="form.name"
-                type="text"
-                autocomplete="off"
-              />
-              <InputError :message="form.name" class="mt-2" />
+            <div class="flex sm:flex-wrap md:flex-nowrap space-x-2">
+              <div class="w-full md:w-1/2">
+                <InputLabel for="kw" value="kW" />
+                <TextInput
+                  id="kw"
+                  v-model="form.kw"
+                  type="text"
+                  autocomplete="off"
+                />
+                <InputError :message="form.errors.kw" />
+              </div>
+              <div class="w-full md:w-1/2">
+                <InputLabel for="a" value="A" />
+                <TextInput
+                  id="a"
+                  v-model="form.a"
+                  type="text"
+                  autocomplete="off"
+                />
+                <InputError :message="form.errors.a" />
+              </div>
+            </div>
+            <div class="flex justify-around">
+              <Link
+                :href="route('charge.directive.index')"
+                class="btn btn-success inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-secondary text-white bg-secondary hover:bg-hover-secondary hover:border-hover-success duration-300 btn-xxs shadow"
+              >
+                Cancelar
+              </Link>
+              <button
+                class="btn btn-success inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-success text-white bg-success hover:bg-hover-success hover:border-hover-success duration-300 btn-xxs shadow"
+              >
+                Crear Tablero de distribuición
+              </button>
             </div>
           </div>
-          <div class="flex sm:flex-wrap md:flex-nowrap space-x-2">
-            <div class="w-full md:w-1/2">
-              <InputLabel for="kw" value="kW" />
-              <TextInput
-                id="kw"
-                v-model="form.kw"
-                type="text"
-                autocomplete="off"
-              />
-              <InputError :message="form.kw" class="mt-2" />
-            </div>
-            <div class="w-full md:w-1/2">
-              <InputLabel for="a" value="A" />
-              <TextInput
-                id="a"
-                v-model="form.a"
-                type="text"
-                autocomplete="off"
-              />
-              <InputError :message="form.a" class="mt-2" />
-            </div>
-          </div>
-          <div class="flex justify-around">
-            <Link
-              :href="route('rooms.room.show', { room: 1 })"
-              class="btn btn-success inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-secondary text-white bg-secondary hover:bg-hover-secondary hover:border-hover-success duration-300 btn-xxs shadow"
-            >
-              Cancelar
-            </Link>
-            <button
-              class="btn btn-success inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-success text-white bg-success hover:bg-hover-success hover:border-hover-success duration-300 btn-xxs shadow"
-            >
-              Crear carga derivada
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   </AuthenticatedLayout>
