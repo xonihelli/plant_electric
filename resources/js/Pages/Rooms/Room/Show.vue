3ac37467a68d609e2445af4f5c9d2be67974bd71
Lucía huerta <luciaxonihelli@gmail.com>
2024-04-14 05:11:37 -0600

    UPDATE: se agrega posicion de paginas (breadcrumbs) y se invoca componente de room

--- resources/js/Pages/Rooms/Room/Show.vue
+++ resources/js/Pages/Rooms/Room/Show.vue
@@ -2,8 +2,9 @@
 <script setup>
 import ChargeDerivateIndex from "@/Pages/Charges/ChargeDerivate/Index.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import ElectricCharge from "@/Pages/Rooms/Components/ElectricCharge.vue";
-import { Link } from "@inertiajs/vue3";
+import ElectricCharge from "@/Pages/Rooms/ElectricCharge/Index.vue";
+import { Link, Head, usePage } from "@inertiajs/vue3";
+import CardRoom from "@/Components/Custom/CardRoom.vue";
 
 const props = defineProps({
   room: {
@@ -37,89 +38,37 @@ let status = [
 ];
 </script>
 <template>
+<Head :title="`Cuarto: ${room.name}`" />
   <AuthenticatedLayout>
-    <div class="flex justify-end mb-4">
-      <Link
-        class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
-        :href="route('rooms.room.index')"
-      >
+    <div class="page-titles dark:bg-[#242424] flex items-center justify-between relative border-b border-[#E6E6E6] dark:border-[#444444] flex-wrap z-[1] py-[0.6rem] sm:px-[1.95rem] px-[1.55rem] bg-white shadow mb-5">
+        <ol class="text-[13px] flex items-center flex-wrap bg-transparent">
+            <li>
+                <Link
+                    :href="route('rooms.room.index')"
+                    class="text-[#828690] dark:text-white text-[13px]
+                ">
+                    Cuartos
+                </Link>
+            </li>
+            <li
+                class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left before:text-primary text-primary font-medium"
+            >
+                {{ room.name }}
+            </li>
+        </ol>
+
+
+        <Link
+            class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
+            :href="route('rooms.room.index')"
+        >
         <i class="fa-solid fa-arrow-left"></i>
         Regresar
       </Link>
     </div>
-    <div class="card">
-      <div
-        class="card-header flex justify-between px-5 pt-6 relative z-[2] pb-0"
-      >
-        <h2 class="card-title text-base">Detalles del cuarto electrico</h2>
 
-        <Link
-          class="btn btn-info inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-info text-white bg-info hover:bg-hover-info hover:border-hover-info duration-300 btn-xxs shadow"
-          :href="route('rooms.room.edit', { room: props.idRoom })"
-        >
-          <i class="fa-solid fa-pen"></i>
-          Editar
-        </Link>
-      </div>
-      <div class="card-body sm:p-5 p-4 sm:pb-0 pb-0">
-        <div class="flex flex-wrap w-full">
-          <div class="w-full md:w-1/2">
-            <h1 class="mb-4 text-md font-semibold">
-              <i class="fa-solid fa-house mr-2"></i>
-              {{ room.name }}
-            </h1>
-            <p class="mb-4">
-              Descripción: <br />
-              {{ props.room.description }}
-            </p>
-          </div>
-          <div class="w-full md:w-1/2">
-            <ul class="list-group flex flex-col list-group-flush">
-              <li
-                class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
-              >
-                <strong> Total de tableros </strong>
-                <span class="mb-0">
-                  CANTIDAD DE Trasnformadores QUE HAY EN UN CUARTO
-                </span>
-              </li>
-              <li
-                class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
-              >
-                <strong> Uso </strong>
-                <span class="mb-0"> USO EN PORCENTAJE 60 % </span>
-              </li>
-              <li
-                class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
-              >
-                <strong> Total de los Kw </strong>
-                <span class="mb-0">TOTAL DE KW </span>
-              </li>
-              <li
-                class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
-              >
-                <strong>Total de los A </strong>
-                <span class="mb-0"> TOTAL DE A</span>
-              </li>
-              <li
-                class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
-              >
-                <strong> Estado </strong>
+    <CardRoom :room="room" :idRoom="idRoom"/>
 
-                <span class="mb-0">
-                  <span
-                    class="py-[0.1875rem] px-[0.8125rem] text-xs rounded-[1.25rem] text-white leading-[1.5] inline-block border duration-500"
-                  >
-                    <!-- :class="`bg-${room.status.class} border-${room.status.class}`" -->
-                    ESTATUS DEL CUARTO
-                  </span>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
     <div class="card dz-tab-area">
       <div
         class="card-header sm:flex justify-between items-center px-5 pt-6 relative z-[2] pb-0 block"
@@ -133,7 +82,7 @@ let status = [
           class="btn btn-success inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-success text-white bg-success hover:bg-hover-success hover:border-hover-success duration-300 btn-xxs shadow"
         >
           <i class="fa-solid fa-plus"></i>
-          Agregar
+          Agregar tablero
         </Link>
       </div>
       <div class="sm:p-5 p-4">
