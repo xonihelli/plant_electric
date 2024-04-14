8bc9c79559890c18b29ef236d6708cf89eee4f56
Erik Gomez <erikgomez398@gmail.com>
2024-04-14 17:24:04 -0600

    Actualizar componentes CardRoom y CardElectronic para mostrar detalles sobre el cuarto eléctrico, el transformador y los tableros de distribución

--- resources/js/Pages/Charges/ChargeDerivate/Show.vue
+++ resources/js/Pages/Charges/ChargeDerivate/Show.vue
@@ -1,75 +1,97 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import SubChargeDerivateIndex from "@/Pages/Charges/ChargeSubDerivate/Index.vue";
+import ChargeDerivateIndex from "@/Pages/Charges/ChargeDerivate/Index.vue";
 import { Link, router } from "@inertiajs/vue3";
 import CardRoom from "@/Components/Custom/CardRoom.vue";
 import CardElectronic from "@/Components/Custom/CardElectronic.vue";
 
-let room = {
-    id: 1,
-    name: "cuarto #" + Math.floor(Math.random() * 1000),
-    description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    charge: Math.floor(Math.random() * 1000),
-    porcent: Math.floor(Math.random() * 100),
-};
-
-let charges = {
-    id: 1,
-    name: "Derivada #" + Math.floor(Math.random() * 1000),
-    description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    charge: Math.floor(Math.random() * 1000),
-    porcent: Math.floor(Math.random() * 100),
-};
-
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({}),
+  },
+  charge: {
+    type: Object,
+    default: () => ({}),
+  },
+  room: {
+    type: Object,
+    default: () => ({}),
+  },
+  idRoom: {
+    type: String,
+  },
+  totalA: {
+    type: Number,
+  },
+  totalKw: {
+    type: Number,
+  },
+  totalTransformers: {
+    type: Number,
+  },
+  totalTablersDistro: {
+    type: Number,
+  },
+});
 </script>
 <template>
   <AuthenticatedLayout>
-    <div class="page-titles dark:bg-[#242424] flex items-center justify-between relative border-b border-[#E6E6E6] dark:border-[#444444] flex-wrap z-[1] py-[0.6rem] sm:px-[1.95rem] px-[1.55rem] bg-white shadow mb-5">
-        <ol class="text-[13px] flex items-center flex-wrap bg-transparent">
-            <li>
-                <Link
-                    :href="route('rooms.room.index')"
-                    class="text-[#828690] dark:text-white text-[13px]
-                ">
-                    Cuartos
-                </Link>
-            </li>
-            <li
-                class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left font-medium"
-            >
-                {{ room.name }}
-            </li>
-            <li
-                class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left before:text-primary text-primary font-medium"
-            >
-                Detalles de tablero: {{ room.name }}
-            </li>
-        </ol>
-
-
-        <Link
-            class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
+    <div
+      class="page-titles dark:bg-[#242424] flex items-center justify-between relative border-b border-[#E6E6E6] dark:border-[#444444] flex-wrap z-[1] py-[0.6rem] sm:px-[1.95rem] px-[1.55rem] bg-white shadow mb-5"
+    >
+      <ol class="text-[13px] flex items-center flex-wrap bg-transparent">
+        <li>
+          <Link
             :href="route('rooms.room.index')"
+            class="text-[#828690] dark:text-white text-[13px]"
+          >
+            Cuartos
+          </Link>
+        </li>
+        <li
+          class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left font-medium"
         >
+          {{ room.name }}
+        </li>
+        <li
+          class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left before:text-primary text-primary font-medium"
+        >
+          Detalles del transformador eléctrico: {{ data.name }}
+        </li>
+      </ol>
+
+      <Link
+        class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
+        :href="route('rooms.room.show', { room: idRoom })"
+      >
         <i class="fa-solid fa-arrow-left"></i>
         Regresar
       </Link>
     </div>
 
     <!-- Cuarto -->
-    <CardRoom :room="room" :edit="false" />
+    <CardRoom
+      :room="room"
+      :idRoom="idRoom"
+      :totalTransformers="totalTransformers"
+      :totalKw="totalKw"
+      :totalA="totalA"
+      :edit="false"
+    />
 
     <!-- Cuarto -->
 
     <!-- Derivada -->
-    <CardElectronic :data="charges" :edit="false" />
+    <CardElectronic
+      :data="data"
+      :totalTablersDistro="totalTablersDistro"
+      :edit="false"
+    />
     <!-- Derivada -->
 
     <!-- Sub derivadas -->
-    <SubChargeDerivateIndex/>
-    <!-- Sub derivadas -->
+    <ChargeDerivateIndex />
   </AuthenticatedLayout>
 </template>
 
