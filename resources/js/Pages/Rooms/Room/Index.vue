69eb1ac35ef09cc077c699652bad1adf6fed7893
Lucía huerta <luciaxonihelli@gmail.com>
2024-04-14 05:09:39 -0600

    UPDATE: cambio de diseño de index de cuartos

--- resources/js/Pages/Rooms/Room/Index.vue
+++ resources/js/Pages/Rooms/Room/Index.vue
@@ -1,103 +1,94 @@
-
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Link } from "@inertiajs/vue3";
+import CardRoom from "@/Components/Custom/CardRoom.vue";
+import CardElectronic from "@/Components/Custom/CardElectronic.vue";
+
 
 const props = defineProps({
-  data: {
-    type: Object,
-    default: () => ({}),
-  },
+    data: {
+        type: Object,
+        default: () => ({}),
+    },
 });
 
 let status = [
-  {
-    id: 1,
-    name: "success",
-    icon: "fa-solid fa-check",
-  },
-  {
-    id: 1,
-    name: "warning",
-    icon: "fa-solid fa-triangle-exclamation",
-  },
-  {
-    id: 1,
-    name: "danger",
-    icon: "fa-solid fa-xmark",
-  },
+    {
+        id: 1,
+        name: "success",
+        icon: "fa-solid fa-check",
+    },
+    {
+        id: 1,
+        name: "warning",
+        icon: "fa-solid fa-triangle-exclamation",
+    },
+    {
+        id: 1,
+        name: "danger",
+        icon: "fa-solid fa-xmark",
+    },
 ];
 </script>
 <template>
-  <AuthenticatedLayout>
-    <div class="row">
-      <div class="xl:w-1/4 lg:w-1/2 sm:w-1/2">
-        <Link :href="route('rooms.room.create')">
-          <div class="card">
-            <div class="card-body depostit-card p-5">
-              <div
-                class="depostit-card-media flex justify-between relative z-[2]"
-              >
-                <h6 class="font-bold">Agregar cuarto</h6>
+    <AuthenticatedLayout>
 
-                <div
-                  class="icon-box h-[2.5rem] w-[2.5rem] relative flex items-center justify-center rounded-md"
-                  :class="`bg-info-light`"
-                >
-                  <i class="fas fa-plus"></i>
-                </div>
-              </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            <div>
+                <Link :href="route('rooms.room.create')">
+                    <div class="card shadow-lg hover:shadow ease-in duration-200">
+                        <div class="card-body depostit-card p-5 flex flex-wrap justify-center items-center h-full w-full">
+                              <h6 class="font-bold block w-full text-center">Agregar cuarto</h6>
+
+                              <div
+                                  class="icon-box h-[2.5rem] w-[2.5rem] relative flex items-center justify-center rounded-md"
+                                  :class="`bg-info-light`"
+                              >
+                                  <i class="fas fa-plus"></i>
+                              </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
-          </div>
-        </Link>
-      </div>
-      <div
-        class="xl:w-1/4 lg:w-1/2 sm:w-1/2"
-        v-for="room in data"
-        :key="room.id"
-      >
-        <div class="card">
-          <div class="card-body depostit-card p-5">
-            <div
-              class="depostit-card-media flex justify-between relative z-[2]"
-            >
-              <Link :href="route('rooms.room.show', room.id)">
-                <h6 class="font-bold">
-                  {{ room.name }}
-                </h6>
-                <h3 class="font-semibold leading-[1.346]">
-                  <!-- {{ room.charge }} -->
-                  <span class="text-[0.75rem]">100k</span>
-                </h3>
-              </Link>
-              <!-- <div
-                class="icon-box h-[2.5rem] w-[2.5rem] relative flex items-center justify-center rounded-md"
-                :class="`bg-${room.status.name}-light`"
-              >
-                <i :class="room.status.icon"></i>
-              </div> -->
+            <div v-for="room in data" :key="room.id">
+                <Link :href="route('rooms.room.show', room.id)">
+                    <div class="card shadow-lg hover:shadow ease-in duration-200">
+                        <div class="card-body depostit-card p-5">
+                            <div
+                                class="depostit-card-media flex justify-between relative z-[2] items-center mb-3"
+                            >
+                                <h5 class="font-bold text-xl">
+                                    {{ room.name }}
+                                </h5>
+
+                                <div
+                                    class="icon-box h-[2.5rem] w-[2.5rem] relative flex items-center justify-center rounded-md"
+                                    :class="`bg-info-light`"
+                                >
+                                    <i class="fas fa-eye"></i>
+                                </div>
+                            </div>
+                            <p class="text-gray-500 flex justify-between">
+                                <b> Carga total </b>
+
+                                <span> 90 </span>
+                            </p>
+                            <p class="text-gray-500 flex justify-between">
+                                <b> Cant. Transformadores </b>
+
+                                <span> 90 </span>
+                            </p>
+                            <p class="text-gray-500 flex justify-between">
+                                <b> Cant. Cargas electricas </b>
+
+                                <span> 90 </span>
+                            </p>
+                        </div>
+                    </div>
+                </Link>
             </div>
-            <div class="progress-box mt-0">
-              <div class="flex justify-between">
-                <p class="mb-0">Porcentaje de uso</p>
-                <p class="mb-0">%/80%</p>
-              </div>
-              <div class="progress h-[5px] flex overflow-hidden rounded-md">
-                <!-- <div
-                  class="progress-bar"
-                  :class="`bg-${room.status.name}`"
-                  style="border-radius: 4px"
-                  :style="`width: 80%;`"
-                  role="progressbar"
-                ></div> -->
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </AuthenticatedLayout>
+    </AuthenticatedLayout>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
