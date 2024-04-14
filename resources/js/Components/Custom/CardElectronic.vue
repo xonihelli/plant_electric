6dfba2bb0b9052a20c306884c5cc6c9e5ffa9c25
Lucía huerta <luciaxonihelli@gmail.com>
2024-04-14 05:07:37 -0600

    ADD: componentes de cuartos y tableros

---
+++ resources/js/Components/Custom/CardElectronic.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card">
+        <div
+          class="card-header flex justify-between px-5 pt-6 relative z-[2]"
+        >
+          <h2 class="card-title text-base">Detalles de tablero electrico</h2>
+        </div>
+        <div class="card-body sm:p-5 p-4">
+          <div class="flex flex-wrap w-full">
+            <div class="w-full md:w-1/2">
+              <h1 class="mb-4 text-md font-semibold text-3xl">
+                <i class="fa-solid fa-bolt mr-2"></i>
+                {{ data.name }}
+              </h1>
+            </div>
+            <div class="w-full md:w-1/2">
+              <ul class="list-group flex flex-col list-group-flush">
+                <li
+                  class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
+                >
+                  <strong> Total de transformadores </strong>
+                  <span class="mb-0">
+                    30
+                  </span>
+                </li>
+                <li
+                  class="list-group-item flex py-4 text-body-color dark:text-white text-sm justify-between"
+                >
+                  <strong> Total de los Kw </strong>
+                  <span class="mb-0">TOTAL DE KW </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+defineProps({
+    data: {
+        type: Object,
+    },
+    idRoom: {
+        type: Number,
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
