6999b260c09304a74745acbc86f4e8e2ece0034f
Lucía huerta <luciaxonihelli@gmail.com>
2024-04-20 12:39:27 -0600

    UPDATE: se agrega bedcrum

--- resources/js/Pages/Charges/ChargeSubDerivate/Edit.vue
+++ resources/js/Pages/Charges/ChargeSubDerivate/Edit.vue
@@ -85,6 +85,43 @@ const sendForm = () => {
 
 <template>
   <AuthenticatedLayout>
+    <div
+      class="page-titles dark:bg-[#242424] flex items-center justify-between relative border-b border-[#E6E6E6] dark:border-[#444444] flex-wrap z-[1] py-[0.6rem] sm:px-[1.95rem] px-[1.55rem] bg-white shadow mb-5"
+    >
+      <ol class="text-[13px] flex items-center flex-wrap bg-transparent">
+        <li>
+          <Link
+            :href="route('room.index')"
+            class="text-[#828690] dark:text-white text-[13px]"
+          >
+            Cuartos
+          </Link>
+        </li>
+        <li
+          class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left font-medium"
+        >
+          {{ room.name }}
+        </li>
+        <li
+          class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left before:text-primary text-primary font-medium"
+        >
+          Detalles del transformador eléctrico: {{ data.name }}
+        </li>
+        <li
+          class="pl-2 before:content-['/'] before:font-[simple-line-icons] before:font-black before:text-xl before:leading-4 before:pr-2 before:float-left before:text-primary text-primary font-medium"
+        >
+          Editar carga: {{ data.name }}
+        </li>
+      </ol>
+
+      <Link
+        class="btn btn-primary inline-block rounded font-medium py-1.5 px-[0.9375rem] text-[0.6875rem] leading-[1.3] border border-primary text-white bg-primary hover:bg-hover-primary hover:border-hover-primary duration-300 btn-xxs shadow"
+        :href="route('room.show', { room: idRoom })"
+      >
+        <i class="fa-solid fa-arrow-left"></i>
+        Regresar
+      </Link>
+    </div>
     <CardRoom
       :room="room"
       :totalTransformers="totalTransformers"
